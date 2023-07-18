b981da77bad755f9499bc2301ecbaf0c99ce100c
jagnat <jagnat4000@gmail.com>
2023-07-18 16:49:12 -0700

    More work on parts list panel, solo/mute button LAF

--- src/com/digero/maestro/view/PartsList.java
+++ src/com/digero/maestro/view/PartsList.java
@@ -1,9 +1,9 @@
 package com.digero.maestro.view;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListModel;
-import javax.swing.ListSelectionModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 import javax.swing.event.ListSelectionListener;
@@ -12,6 +12,7 @@ import com.digero.common.util.IDiscardable;
 import com.digero.maestro.abc.AbcPart;
 import com.digero.maestro.abc.AbcPartMetadataSource;
 
+import info.clearthought.layout.TableLayout;
 import info.clearthought.layout.TableLayoutConstants;
 
 public class PartsList extends JPanel implements IDiscardable, TableLayoutConstants, ListDataListener
@@ -22,11 +23,27 @@ public class PartsList extends JPanel implements IDiscardable, TableLayoutConsta
 	private static final long serialVersionUID = 3832994437250655508L;
 	
 	private static double[] LAYOUT_COLS = new double[] {FILL};
+	private static double[] LAYOUT_ROWS = new double[] {PREFERRED, PREFERRED, PREFERRED, PREFERRED};
 	
 	private ListModel<AbcPartMetadataSource> model;
+	private TableLayout layout;
 	
 	public PartsList()
 	{
+		super(new TableLayout(LAYOUT_COLS, LAYOUT_ROWS));
+		
+		layout = (TableLayout)getLayout();
+		
+		for (int i = 0; i < LAYOUT_ROWS.length; i++)
+		{
+			add(new PartsListItem(), "0, " + i);
+		}
+		
+		PartsListItem selected = new PartsListItem();
+		selected.setSelected(true);
+		layout.insertRow(2, PREFERRED);
+		add(selected, "0, 2");
+		
 		model = new DefaultListModel<AbcPartMetadataSource>();
 	}
 	
--- src/com/digero/maestro/view/PartsListItem.java
+++ src/com/digero/maestro/view/PartsListItem.java
@@ -1,13 +1,14 @@
 package com.digero.maestro.view;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 
 import com.digero.common.util.IDiscardable;
 
@@ -16,14 +17,7 @@ import info.clearthought.layout.TableLayoutConstants;
 
 public class PartsListItem extends JPanel implements IDiscardable, TableLayoutConstants
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1794798972919435415L;
-	private static final int GUTTER_COLUMN = 0;
-	private static final int TITLE_COLUMN = 1;
-	private static final int SOLO_COLUMN = 2;
-	private static final int MUTE_COLUMN = 3;
 	
 	static final int GUTTER_WIDTH = 4;
 	static final int TITLE_WIDTH = 100;
@@ -38,6 +32,9 @@ public class PartsListItem extends JPanel implements IDiscardable, TableLayoutCo
 	private JButton soloButton;
 	private JButton muteButton;
 	
+	private boolean isSolo = false;
+	private boolean isMute = false;
+	
 	public PartsListItem()
 	{
 		super(new TableLayout(LAYOUT_COLS, LAYOUT_ROWS));
@@ -46,23 +43,41 @@ public class PartsListItem extends JPanel implements IDiscardable, TableLayoutCo
 		
 		title = new JLabel("This is test");
 		
-		soloButton = new JButton("<html><b>S</b></html>");
+		soloButton = new JButton("S");
 		soloButton.setPreferredSize(new Dimension(22, 22));
 		soloButton.setMargin( new Insets(0, 0, 0, 0));
 		soloButton.setFocusable(false);
+		soloButton.addActionListener(e -> {
+			isSolo = !isSolo;
+			soloButton.setBackground(isSolo? Color.decode("#aaaaff") : new JButton().getBackground());
+			soloButton.setText(isSolo? "<html><b>S</b></html>" : "S");
+		});
 		
 		muteButton = new JButton("M");
 		muteButton.setPreferredSize(new Dimension(22, 22));
 		muteButton.setMargin( new Insets(0, 0, 0, 0));
 		muteButton.setFocusable(false);
+		muteButton.addActionListener(e -> {
+			isMute = !isMute;
+			muteButton.setBackground(isMute? Color.decode("#ff7777") : new JButton().getBackground());
+			muteButton.setText(isMute? "<html><b>M</b></html>" : "M");
+		});
 		
-		add(gutter, GUTTER_COLUMN + ", 0");
-		add(title, TITLE_COLUMN + ", 0");
-		add(soloButton, SOLO_COLUMN + ", 0");
-		add(muteButton, MUTE_COLUMN + ", 0");
-		JList list = new JList();
-		
-		setBackground(list.getSelectionBackground());
+		int col = -1;
+		add(gutter, ++col + ", 0");
+		add(title, ++col + ", 0");
+		add(soloButton, ++col + ", 0");
+		add(muteButton, ++col + ", 0");
+	}
+	
+	void setSelected(boolean selected)
+	{
+		JList dummy = new JList();
+		Color bg = selected? dummy.getSelectionBackground() : dummy.getBackground();
+		Color fg = selected? dummy.getSelectionForeground() : dummy.getForeground();
+		setBackground(bg);
+		setForeground(fg);
+//		title.setText("<html><b><u>This is a test</u></b></html>");
 	}
 	
 	@Override
