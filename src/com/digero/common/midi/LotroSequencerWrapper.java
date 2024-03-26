jagnat/maestro

commit ca7cb1d41100bb690f4590089a4a35552287cdb6

Fix not playing initial notes, when starting Maestro with a MSX association.

src/com/digero/common/midi/LotroSequencerWrapper.java
@@ -25,18 +25,27 @@ public class LotroSequencerWrapper extends NoteFilterSequencerWrapper {
 		}
 	}
 
+	/**
+	 * Only affects channels larger than 16.
+	 * Will stop all active notes. And then inject a patch change to each channel
+	 * since java don't do that properly with 16+ channels.
+	 * 
+	 * @param doControllers Stop all midi controllers also
+	 */
 	public void injectPatchChanges(boolean doControllers) {
 		List<ExportTrackInfo> infos = SequenceInfo.lastTrackInfos;
 		if (infos != null) {
 			for (ExportTrackInfo info : infos) {
-				receiver.send(MidiFactory.createAllNotesOff(info.channel), -1L);
-				receiver.send(MidiFactory.createSustainOff(info.channel), -1L);// -1 means real-time
-				if (doControllers) {
-					// We only do this when stopping to play
-					receiver.send(MidiFactory.createAllControllersOff(info.channel), -1L);
+				if (info.channel + 1 > MidiConstants.CHANNEL_COUNT) {
+					receiver.send(MidiFactory.createAllNotesOff(info.channel), -1L);
+					receiver.send(MidiFactory.createSustainOff(info.channel), -1L);// -1 means real-time
+					if (doControllers) {
+						// We only do this when stopping to play
+						receiver.send(MidiFactory.createAllControllersOff(info.channel), -1L);
+					}
+					receiver.send(MidiFactory.createLotroChangeEvent(info.patch, info.channel, sequencer.getTickPosition())
+							.getMessage(), -1L);
 				}
-				receiver.send(MidiFactory.createLotroChangeEvent(info.patch, info.channel, sequencer.getTickPosition())
-						.getMessage(), -1L);
 			}
 		}
 	}

commit fd90cd72888b0eef595f239b964b3b0d2c8f6629

Semi-fix regression introduced with commit ca7cb1d41100bb690f4590089a4a35552287cdb6 that sometimes with 15+ parts, preview would sometimes select the wrong patch especially after use of mute/solo features.

src/com/digero/common/midi/LotroSequencerWrapper.java
@@ -34,9 +34,8 @@ public class LotroSequencerWrapper extends NoteFilterSequencerWrapper {
 	 */
 	public void injectPatchChanges(boolean doControllers) {
 		List<ExportTrackInfo> infos = SequenceInfo.lastTrackInfos;
-		if (infos != null) {
+		if (infos != null && infos.size() > MidiConstants.CHANNEL_COUNT-1) {
 			for (ExportTrackInfo info : infos) {
-				if (info.channel + 1 > MidiConstants.CHANNEL_COUNT) {
 					receiver.send(MidiFactory.createAllNotesOff(info.channel), -1L);
 					receiver.send(MidiFactory.createSustainOff(info.channel), -1L);// -1 means real-time
 					if (doControllers) {
@@ -45,7 +44,6 @@ public class LotroSequencerWrapper extends NoteFilterSequencerWrapper {
 					}
 					receiver.send(MidiFactory.createLotroChangeEvent(info.patch, info.channel, sequencer.getTickPosition())
 							.getMessage(), -1L);
-				}
 			}
 		}
 	}